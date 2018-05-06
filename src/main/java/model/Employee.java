69b1064d1a57519c5eab7b6ec2e58b5b3303a675
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-05-06 12:38:02 +0400

    Update Employee.java

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -23,36 +23,73 @@ public class Employee {
     @JoinColumn(name = "department_name", referencedColumnName = "name")
     private Department department;
 
+    /**
+     * This is getter for Department "department" field.
+     *@return Department "department" field.
+     *@see Department
+     */
     public Department getDepartment() { return department; }
 
+    /**
+     * This is setter for Department "department" field.
+     *@see Department
+     */
     public void setDepartment(Department department) { this.department = department; }
 
+    /**
+     * This is getter for String "nameAndFamily" field.
+     *@return String "nameAndFamily" field.
+     */
     public String getNameAndFamily() {
         return nameAndFamily;
     }
 
+    /**
+     * This is setter for String "nameAndFamily" field.
+     */
     public void setNameAndFamily(String nameAndFamily) {
         this.nameAndFamily = nameAndFamily;
     }
 
+    /**
+     * This is getter for String "departmentName" field.
+     *@return String "departmentName" field.
+     */
     public String getDepartmentName() { return departmentName; }
 
+    /**
+     * This is setter for String "departmentName" field.
+     */
     public void setDepartmentName(String departmentName) {
         this.departmentName = departmentName;
     }
 
+    /**
+     * This is getter for String "phone" field.
+     *@return String "phone" field.
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * This is setter for String "phone" field.
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    /**
+     * This is getter for double "salary" field.
+     *@return double "phone" field.
+     */
     public double getSalary() {
         return salary;
     }
 
+    /**
+     * This is setter for double "salary" field.
+     */
     public void setSalary(double salary) {
         this.salary = salary;
     }
