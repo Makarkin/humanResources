dd8cda7a44327d8579e2fc1c00e79b71bb470e15
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-05-06 12:32:38 +0400

    Update ApplicationMain.java

--- src/main/java/application/ApplicationMain.java
+++ src/main/java/application/ApplicationMain.java
@@ -6,6 +6,11 @@ import model.SQLFunction;
 import java.util.logging.Level;
 
 public class ApplicationMain {
+        /**
+     *This is main method with created exemplar "ControllerClass".
+     * It starts console application and create query to database.
+     * @see ControllerClass
+     */
     public static void main(String[] args) {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         ControllerClass controllerClass = new ControllerClass();
